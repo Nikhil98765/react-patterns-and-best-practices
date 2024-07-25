33a7bdbf329c50beae26d33d9e9189506107a39b
Nikhil98765 <42944335+Nikhil98765@users.noreply.github.com>
2024-07-25 23:57:47 +0530

    Adding extra components for reusability && sharing cross component state when working with compound components

--- src/components/AccordionItem.jsx
+++ src/components/AccordionItem.jsx
@@ -1,22 +1,23 @@
-import { useAccordionContext } from "./Accordion"
+import { createContext, useContext } from "react";
 
 
-export const AccordionItem = ({ id, children, className, title }) => {
-  
-  const { openItemId, toggleItem } = useAccordionContext();
+const AccordionItemContext = createContext();
 
-  const isOpen = openItemId === id;
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext);
 
+  if (!ctx) {
+    throw new Error(
+      "AccordionItem related components must be wrapped by <AccordionItem>."
+    );
+  }
+  return ctx;
+}
+
+export const AccordionItem = ({ id, children, className }) => {
   return (
-    <li className={className}>
-      <h3 onClick={() => toggleItem(id)}>{title}</h3>
-      <div
-        className={
-          isOpen ? "accordion-item-content open" : "accordion-item-content"
-        }
-      >
-        {children}
-      </div>
-    </li>
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
   );
 }
